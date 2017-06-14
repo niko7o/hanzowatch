7bed9897bfab6c266a384ee92dfebbc06ef78825
Nikolaas Verlee <nikotomad@gmail.com>
2017-06-14 18:59:52 +0200

    Cambios estructura

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<title>Hanzowatch</title>
 	<link rel="stylesheet" href="css/estilos.css">
     <meta charset="UTF-8">
-    <meta name="description" content="overwatch tracker">
+    <meta name="description" content="opensource overwatch tracker">
     <meta name="keywords" content="HTML,CSS,PHP,JavaScript">
     <meta name="author" content="Nikolaas Verlee">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,131 +13,118 @@
 </head>
     
 <body>
-    <!-- particles.js container
-    <div id="particles-js"></div>
-    -->
+    <!-- Navigation -->
     <nav>
         <img src="img/logo.png" id="logo" alt="Hanzowatch" />
         <ul>
             <li><a href="index.php">Home</a></li>
             <li><a href="/forums" target="_blank">Forums</a></li>
-            <li><a href="sitenews.php">Site news</a></li>
-            <li><a href="account.php">Account</a></li>
+            <li><a href="patchnotes.php">Patchnotes</a></li>
+            <li><a href="counters.php">Counters</a></li>
         </ul>
     </nav>
     
-    <div class="centrado960" style="background-color:#e6e6e6;">
-        <h3 style="color:gray">Use - instead of # when searching your BattleTag.</h3>
+    <!-- Player search form -->
+    <div class="centrado960">
+        <?php
+            $player = $_GET['player']; //recogemos el battletag dado
+            $region = 'eu'; //we only work with Europe until playerbase extends
+        ?>
+        <h3 id="variableText">
+            <?php
+            if(!isset($player)){ //control flow if they come from an already-existing query or first query ever
+                echo "Use - instead of # for your BattleTag.";
+            } else {
+                echo "Feel free to look up other players!";
+            }
+            ?>
+        </h3>
         <form id="battletag" action="index.php" method="get">
             BattleTag: <input type="text" name="player" id="battletagplayer" placeholder="User-2122">
         <input type="submit" id="lupa" value="Search">
         </form>
     </div>
-
+    
+    <!-- Image below form-->
     <div class="heroes">
         <img src="img/heroes.png" alt="Overwatch heroes"/>
     </div>
 
-<!-- Battletag search -->
 <?php
-	$player = $_GET['player']; //recogemos el battletag dado
-    $region = 'eu'; //solo trabajamos con region europa por ahora
-
-    $response = get_web_page("http://owapi.net/api/v3/u/".$player."/blob"); //llamada cURL
-    $response = json_decode($response); //descodificamos el objeto devuelto con nuestra llamada
+    $response = requestAPI("http://owapi.net/api/v3/u/".$player."/blob"); //cURL call to the owAPI
+    $response = json_decode($response); //we decode the json object received from requestAPI
     
-	$data = $response; //asignamos el objeto a la variable $data
+	$data = $response; //we assign the object to our $data variable
 
-    //creo atajos para la incrustación de datos dinámicos en mi PHP
+    //we shorten the variables with route shortcuts
 	$competitive_overall_stats = $data->$region->stats->competitive->overall_stats;
 	$competitive_game_stats = $data->$region->stats->competitive->game_stats;
 	$quickplay_overall_stats = $data->$region->stats->quickplay->overall_stats;
 	$quickplay_game_stats = $data->$region->stats->quickplay->game_stats;
 
-    if(isset($player)){ //comprobamos si ha hecho una petición
-        if($data){ //ya sabemos que hay petición. ¿Nos devuelve algo?
+    if(isset($player)){ //do we have a battletag to search for?
 ?>
-            <div id="playerResults">
-                <div id="competitive">
-                    <span class="title">Competitive</span>
-                    <ul>
-                        <li> Games: <b><?php echo $competitive_overall_stats->games ?></b> </li>
-                        <li> Wins: <b><?php echo $competitive_overall_stats->wins ?></b> </li>
-                        <li> Losses: <b><?php echo $competitive_overall_stats->losses ?></b> </li>
-                        <li> ELO Ranking: <b><?php echo $competitive_overall_stats->comprank ?></b> </li>
-                    </ul>
-                </div>
-                <div id="quickplay">
-                    <span class="title">Quickplay</span>
-                    <ul>
-                        <li> Games: <b><?php echo $quickplay_overall_stats->games ?></b> </li>
-                        <li> Wins: <b><?php echo $quickplay_overall_stats->wins ?></b> </li>
-                        <li> Losses: <b><?php echo $quickplay_overall_stats->losses ?></b> </li>
-                        <li> Winrate: <b><?php echo $quickplay_overall_stats->win_rate ?>%</b></li>
-                    </ul>
-                </div>
+        <div id="playerResults">
+            <div id="competitive">
+                <span class="title">Competitive</span>
+                <ul>
+                    <li> Games: <b><?php echo $competitive_overall_stats->games ?></b> </li>
+                    <li> Wins: <b><?php echo $competitive_overall_stats->wins ?></b> </li>
+                    <li> Losses: <b><?php echo $competitive_overall_stats->losses ?></b> </li>
+                    <li> ELO Ranking: <b><?php echo $competitive_overall_stats->comprank ?></b> </li>
+                </ul>
             </div>
-
-            <div id="gameResults" style="display:none">
-                <div id="avatar"></div>
-                <div id="champion">
-                    <h1>general game statistics</h1>
-                </div>
-                <div id="data"></div>
+            <div id="quickplay">
+                <span class="title">Quickplay</span>
+                <ul>
+                    <li> Games: <b><?php echo $quickplay_overall_stats->games ?></b> </li>
+                    <li> Wins: <b><?php echo $quickplay_overall_stats->wins ?></b> </li>
+                    <li> Losses: <b><?php echo $quickplay_overall_stats->losses ?></b> </li>
+                    <li> Winrate: <b><?php echo $quickplay_overall_stats->win_rate ?>%</b></li>
+                </ul>
             </div>
-
-            <?php
-            //array + foreach para recorrer datos variables de campeones
-            $campeones = array("reinhardt","tracer","zenyatta","junkrat","mccree","winston","orisa","hanzo","pharah","roadhog","zarya","torbjorn","mercy","mei","ana","widowmaker","genji","reaper","soldier76","bastion","symmetra","dva","sombra","lucio");
-
-            //forEach anidado con cada foto de campeon
-            $fotosCampeones = array();
-            
-            foreach ($campeones as $campeon) {
-                echo "<div class='heroResults' id='".$campeon."'>";
-                    echo "<div id='avatar'>";
-                        //foreach anidado imágenes de cada campeon
-                    echo "</div>";
-                    echo "<div id='champion'><h1>" . $campeon . "</h1></div>"; //nombre de cada campeon
-                    
-                    echo "<div id='data'>";
-                        echo "<p> Eliminations: ".$data->$region->heroes->stats->competitive->$campeon->general_stats->eliminations."</p>";
-                        echo "<p> Deaths: ".$data->$region->heroes->stats->competitive->$campeon->general_stats->deaths."</p>";
-                        echo "<p> Total damage: ".$data->$region->heroes->stats->competitive->$campeon->general_stats->damage_done."</p>";
-                        echo "<p style='color:green'> Win ratio: ". 100*$data->$region->heroes->stats->competitive->$campeon->general_stats->win_percentage."%</p>";
-
-                        echo "<p> Games played: ".$data->$region->heroes->stats->competitive->$campeon->general_stats->games_played."</p>";
-                        echo "<p> Games won: ".$data->$region->heroes->stats->competitive->$campeon->general_stats->games_won."</p>";
-                        echo "<p style='color:orange'> K/D: ".$data->$region->heroes->stats->competitive->$campeon->general_stats->eliminations_per_life."</p>";
-                        echo "<p> Most damage: ". $data->$region->heroes->stats->competitive->$campeon->general_stats->damage_done_most_in_game."</p>";
-
-                        echo "<p> Eliminations: ".$data->$region->heroes->stats->competitive->$campeon->general_stats->eliminations."</p>";
-                        echo "<p> Deaths: ".$data->$region->heroes->stats->competitive->$campeon->general_stats->deaths."</p>";
-                        echo "<p> Total damage: ".$data->$region->heroes->stats->competitive->$campeon->general_stats->damage_done."</p>";
-                        echo "<p> Win ratio: ". 100*$data->$region->heroes->stats->competitive->$campeon->general_stats->win_percentage."%</p>";
-                    echo "</div>";
-                echo "</div>";//fin heroResults
-                $campeon++;
-            }
-            ?>
+        </div>
 
         <?php
-        }else { //ha probado una búsqueda pero no hemos recibido respuesta del cURL
-            echo "<div class='centrado960' style='margin-top:2em !important'>The BattleTag entered is incorrect. Try again.</div>";
+            //array + foreach para recorrer datos variables de campeones
+            $champions = array("reinhardt","tracer","zenyatta","junkrat","mccree","winston","orisa","hanzo","pharah","roadhog","zarya","torbjorn","mercy","mei","ana","widowmaker","genji","reaper","soldier76","bastion","symmetra","dva","sombra","lucio");
+            
+            foreach ($champions as $champion) {
+                echo "<div class='hero' id='".$champion."'>";
+                    echo "<div id='avatar'>";
+                        //we load the avatars via css
+                    echo "</div>";
+                    echo "<div id='champion'><h1>" .$champion. "</h1></div>";
+             
+                    echo "<div id='data'>";
+                        echo "<p> Eliminations: ".$data->$region->heroes->stats->competitive->$champion->general_stats->eliminations."</p>";
+                        echo "<p> Deaths: ".$data->$region->heroes->stats->competitive->$champion->general_stats->deaths."</p>";
+                        echo "<p> Total damage: ".$data->$region->heroes->stats->competitive->$champion->general_stats->damage_done."</p>";
+                        echo "<p style='color:green'> Win ratio: ". 100*$data->$region->heroes->stats->competitive->$champion->general_stats->win_percentage."%</p>";
+
+                        echo "<p> Games played: ".$data->$region->heroes->stats->competitive->$champion->general_stats->games_played."</p>";
+                        echo "<p> Games won: ".$data->$region->heroes->stats->competitive->$champion->general_stats->games_won."</p>";
+                        echo "<p style='color:orange'> K/D: ".$data->$region->heroes->stats->competitive->$champion->general_stats->eliminations_per_life."</p>";
+                        echo "<p> Most damage: ". $data->$region->heroes->stats->competitive->$champion->general_stats->damage_done_most_in_game."</p>";
+
+                        echo "<p> Eliminations: ".$data->$region->heroes->stats->competitive->$champion->general_stats->eliminations."</p>";
+                        echo "<p> Deaths: ".$data->$region->heroes->stats->competitive->$champion->general_stats->deaths."</p>";
+                        echo "<p> Total damage: ".$data->$region->heroes->stats->competitive->$champion->general_stats->damage_done."</p>";
+                        echo "<p> Win ratio: ". 100*$data->$region->heroes->stats->competitive->$champion->general_stats->win_percentage."%</p>";
+                    echo "</div>";
+                echo "</div>";
+                $champion++; //we have printed all the info for champìon, let's go to the next one
+            }
         }
-    
-    }else { //No nos ha hecho petición todavía.. Le informamos de ello.
-        echo "<div class='centrado960' style='margin-top:2em !important'>Please enter a BattleTag to search for.</div>";
-    }
-    
-	function get_web_page($url) { //funcion de cURL con sus opciones
+
+	function requestAPI($url) { //funcion de cURL con sus opciones
 	    $options = array(
 	        CURLOPT_RETURNTRANSFER => true,   // return web page
 	        CURLOPT_HEADER         => false,  // don't return headers
 	        CURLOPT_FOLLOWLOCATION => true,   // follow redirects
 	        CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
 	        CURLOPT_ENCODING       => "",     // handle compressed
-	        CURLOPT_USERAGENT      => "CodeNiko Overwatch", // name of client
+	        CURLOPT_USERAGENT      => "Hanzowatch", // name of client
 	        CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
 	        CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
 	        CURLOPT_TIMEOUT        => 120,    // time-out on response
@@ -149,16 +136,11 @@
         $content  = curl_exec($ch);
         curl_close($ch);
 
-    return $content;
+        return $content;
     }
 ?>
 
 <div class="preloader"></div>
-
-<!-- scripts
-<script src="../particles.js"></script>
-<script src="js/app.js"></script>
--->
 
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 $(document).ready(function() {
 	$("#lupa").click(function(){
-		
-		var playerBuscado = $('#battletagplayer').val();
-	    $('.preloader').css('display','block');
+		var player = $('#battletagplayer').val(); //we save the value of battletag input in a variable
+	    $('.preloader').css('display','block'); //we display the preloader as soon as they click
 	    
 	    $.ajax({
 	    	method: "GET",
-	    	url: "http://www.codeniko.com/index.php?player="+playerBuscado,
+	    	url: "http://www.codeniko.com/index.php?player="+player,
+	    	crossDomain: true,
+    		dataType: 'jsonp',
 	    	success: function(){
-	        	$('.preloader').css('display','none');
+	        	$('.preloader').css('display','none'); //once the data is returned, we hide our preloader
 	    	}
 	    });
 	    
