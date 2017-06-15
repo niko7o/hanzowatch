3be4a0098a4dadb7977fa3611392cd3b2506e697
Nikolaas Verlee <nikotomad@gmail.com>
2017-06-15 16:53:13 +0200

    Patchnotes

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
             <ul>
                 <li><a href="index.php">Search</a></li>
                 <li><a href="/forums" target="_blank">Forums</a></li>
-                <li><a href="/patchnotes/index.php">Patchnotes</a></li>
+                <li><a href="https://playoverwatch.com/en-us/game/patch-notes/pc/" target="_blank">Patchnotes</a></li>
                 <li><a href="/counters/index.php">Counters</a></li>
             </ul>
         </div>
